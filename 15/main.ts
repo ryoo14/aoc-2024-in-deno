fc937c79b09033ec7d94461b2115256fd2b01075
ryoo14 <anana12185@gmail.com>
2024-12-18 09:31:16 +0900

    thinking face

---
+++ 15/main.ts
@@ -0,0 +1,75 @@
+export function part1(input: Array<string>) {
+  const grid: string[][] = []
+  let dir: string[] = []
+  let flag = true
+  for (const i of input) {
+    if (i === "") {
+      flag = false
+      continue
+    }
+    
+    if (flag) {
+      grid.push(i.split(""))
+    } else {
+      dir = dir.concat(i.split(""))
+    }
+  }
+
+  let at: number[] = []
+  for (let i = 0; i < grid.length; i++) {
+    for (let l = 0; l < grid[i].length; l++) {
+      if (grid[i][l] === "@") {
+        at.push(i, l)
+        break
+      }
+    }
+  }
+
+  const m = new Map()
+  m.set("^", [-1, 0])
+  m.set(">", [0, 1])
+  m.set("<", [0, -1])
+  m.set("v", [1, 0])
+
+  for (const d of dir) {
+    const dd: number[] = m.get(d)
+    const l: number[][] = []
+    while (1) {
+      const po = grid[at[0]+dd[0]][at[1]+dd[1]]
+      if (po === "#") {
+        break
+      } else if (po === "O") {
+        l.push([at[0]+dd[0], at[1]+dd[1]])
+        at = [at[0]+dd[0], at[1]+dd[1]]
+      } else if (po === ".") {
+        if (l.length !== 0) {
+          grid[at[0]+dd[0]][at[1]+dd[1]] = "O"
+          at = [l[0][0], l[0][1]]
+          grid[at[0]-dd[0]][at[1]-dd[1]] = "."
+          grid[at[0]][at[1]] = "@"
+        } else {
+          grid[at[0]][at[1]] = "."
+          grid[at[0]+dd[0]][at[1]+dd[1]] = "@"
+          at = [at[0]+dd[0], at[1]+dd[1]]
+        }
+        break
+      }
+    }
+    break
+  }
+
+}
+
+export function part2(input: Array<string>) {
+
+}
+
+function parseInput(input: string): Array<string> {
+  return input.trim().split(/\n/)
+}
+
+const rawInput = await Deno.readTextFile(`${import.meta.dirname}/input.txt`)
+const input = parseInput(rawInput)
+
+//console.log(part1(input))
+//console.log(part2(input))
---
+++ 15/main_test.ts
@@ -0,0 +1,38 @@
+import { assertEquals } from "@std/assert";
+const { part1, part2 } = await import(`${import.meta.dirname}/main.ts`)
+
+const exampleInput1 = [
+  "##########",
+  "#..O..O.O#",
+  "#......O.#",
+  "#.OO..O.O#",
+  "#..O@..O.#",
+  "#O#..O...#",
+  "#O..O..O.#",
+  "#.OO.O.OO#",
+  "#....O...#",
+  "##########",
+  "",
+  "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+  "vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+  "><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+  "<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+  "^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+  "^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+  ">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+  "<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+  "^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+  "v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+]
+
+const exampleInput2 = [
+  ""
+]
+
+Deno.test(function part1Test() {
+  assertEquals(part1(exampleInput1), 10092);
+});
+
+Deno.test(function part2Test() {
+  assertEquals(part2(exampleInput2), 0);
+});
